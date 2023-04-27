a2cc9a2e59b13331b32d1222f271bdba3dc61886
yookiyoh <ralphlorenzicodilan@iskolarngbayan.pup.edu.ph>
2023-04-27 18:28:21 +0800

    creating headline comments

---
+++ squared even and cubed odd integers.py
@@ -0,0 +1,8 @@
+# Codilan, Ralph Lorenz I.
+# BSCpE 1-5
+# Object-Oriented Programming | Assignment 4 - Problem 4
+# This program writes a method in Python wherein the extracted even integers will be squared 
+# and the extracted odd integers will be cubed from the main text file 
+# (similar acquisition from Program 1 where extraction of odd and even integers take place)
+# Thus transferred into separate text files per each categorical result.
+
